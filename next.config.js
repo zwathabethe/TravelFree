048f421ee53c37fde266e71951ea97768e9c2ac3
Mzwa Thabethe <mzwa.thabethe@gmail.com>
2025-06-14 15:38:48 +0100

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = nextConfig; 
