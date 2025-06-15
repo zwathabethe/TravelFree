efd18a1262abb1ba95550b2abee4de30fc67568f
Mzwa Thabethe <mzwa.thabethe@gmail.com>
2025-06-15 11:45:47 +0100

    feat: add unsplash domain to next config

--- next.config.js
+++ next.config.js
@@ -10,6 +10,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
